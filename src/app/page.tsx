7a5df4da8fcef0664c166761e6b176196174cc99
yjj <yjj2012uestc@163.com>
2024-11-26 16:56:49 +0800

    feat: 3-3完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ import { Textarea } from "@/components/Textarea"
 
 
 export default function Home() {
-  return <form>
-    <Input></Input>
-    <Textarea></Textarea>
-  </form>
+  return <div className="h-screen flex justify-center items-center">
+    <form className="w-full max-w-md flex flex-col gap-4">
+      <h1 className="text-center text-2xl font-bold">Create App</h1>
+      <Input name="name" placeholder="App Name"></Input>
+      <Textarea name="description" placeholder="Description"></Textarea>
+      <Button>Submit</Button>
+    </form>
+  </div>
 }
